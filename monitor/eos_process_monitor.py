9e88215bef67b20951396f5aaf074e628e678621
zhi.yang <zhi.yang@bitmain.com>
2018-08-09 15:15:32 +0800

    update node monitor

--- monitor/eos_process_monitor.py
+++ monitor/eos_process_monitor.py
@@ -2,56 +2,59 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import logging
 
 import psutil
 
+import init_work_home
 
-class Log:
-    def __init__(self, log):
-        logging.basicConfig(
-            level=logging.INFO,
-            format='%(asctime)s %(levelname)s %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S',
-            filename=log,
-            filemode='a')
-
-    def info(self, message):
-        logging.info(message)
-
-    def error(self, message):
-        logging.error(message)
+init_work_home.init()
+from config.config import Config
+from utils import logger
+from utils.metric import Metric
 
 
 class Monitor:
-    def __init__(self, pid, log):
-        self.__pid = int(pid)
-        self.__log = log
+    def __init__(self, pname):
+        self.pname = str(pname)
+        self.pid = None
+
+    def init_pid(self):
+        try:
+            for p in psutil.process_iter():
+                if p.name().lower() == self.pname.lower():
+                    self.pid = p.pid
+                    break
+        except Exception as e:
+            logger.error(str(e))
+        if self.pid is None:
+            raise Exception
 
     def init_process(self):
-        global pid, process
-        process = psutil.Process(self.__pid)
+        global process
+        self.init_pid()
+        process = psutil.Process(self.pid)
 
     def ram(self):
         ram_usage = process.memory_percent()
         ram_usage = round(ram_usage, 2)
-        self.__log.info("%s %s" % (self.__pid, ram_usage))
-        # add metric collector
+        self.metric_collect("eos_ram", ram_usage)
 
     def cpu(self):
         cpu_usage = process.cpu_percent(interval=1)
         cpu_usage = round(cpu_usage, 2)
-        self.__log.info("%s %s" % (self.__pid, cpu_usage))
-        # add metric collector
+        self.metric_collect("eos_cpu", cpu_usage)
 
     def connections(self):
         connections = psutil.net_connections('tcp')
         connection_count = 0
         for sconn in connections:
-            if self.__pid == sconn.pid:
+            if self.pid == sconn.pid:
                 connection_count = connection_count + 1
-        self.__log.info("%s %s" % (self.__pid, connection_count))
-        # add metric collector
+        self.metric_collect("eos_connections", connection_count)
+
+    def metric_collect(self, key, value):
+        logger.info("%s %s %s" % (self.pid, key, value))
+        Metric.metric(key, value)
 
     def monitor(self):
         try:
@@ -60,19 +63,18 @@ class Monitor:
             self.ram()
             self.connections()
         except Exception as e:
-            self.__log.error(str(e))
+            logger.error(str(e))
 
 
 def usage():
-    global id, log
-    parser = argparse.ArgumentParser(description='process cpu/ram monitor tool.')
-    parser.add_argument('-p', '--pid', required=True, help='process pid')
-    parser.add_argument('-l', '--log', required=True, help='log file')
+    global pname
+    default_name = Config.get_process_name()
+    parser = argparse.ArgumentParser(description='process cpu/ram/connections monitor tool.')
+    parser.add_argument('-n', '--pname', default=default_name, help='process pid')
     args = parser.parse_args()
-    id = args.pid
-    log = args.log
+    pname = args.pname
 
 
 if __name__ == '__main__':
     usage()
-    Monitor(id, Log(log)).monitor()
+    Monitor(pname).monitor()
