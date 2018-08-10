2159e9beb259f6b66350cd45879e2e15b3019caf
zhi.yang <zhi.yang@bitmain.com>
2018-08-10 22:27:58 +0800

    add bp produce check

--- utils/http.py
+++ utils/http.py
@@ -5,28 +5,28 @@ from logger import logger
 def_headers = {'Content-Type': 'application/json'}
 
 
-def get(action, url, params=None, timeout=3.0, log=False):
+def get(action, url, params=None, timeout=3.0, record_response=False):
     try:
         response = requests.get(url, params=params, timeout=timeout)
-        record_response(action, response, log)
+        check_and_record(action, response, record_response)
         return response
     except Exception as e:
         logger.error("%s get exception:%s", action, e)
         raise e
 
 
-def post(action, url, headers=None, timeout=3.0, data=None, params=None, log=False):
+def post(action, url, headers=None, timeout=3.0, data=None, params=None, record_response=False):
     try:
         response = requests.post(url, data=data, params=params, headers=headers, timeout=timeout)
-        record_response(action, response, log)
+        check_and_record(action, response, record_response)
         return response
     except Exception as e:
         logger.error("%s post exception:%s", action, e)
         raise e
 
 
-def record_response(action, response, log=False):
-    result = 'success'
-    if log:
-        result = response.json()
-    logger.info("%s: %s", action, result)
+def check_and_record(action, response, record):
+    if response.status_code != 200:
+        raise Exception(response.text)
+    if record:
+        logger.info("%s: %s", action, response.json())
