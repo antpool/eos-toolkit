228711b0b441e6029a2ee0e8f4fe45f23bab0fae
zhi.yang <zhi.yang@bitmain.com>
2018-08-15 17:12:47 +0800

    upgrade auo claim

--- claim/auto_claim.sh
+++ claim/auto_claim.sh
@@ -15,6 +15,8 @@ init_config() {
     cleos="${eos_client} -u ${api} --wallet-url=${wallet_api}"
 
     last_claim_time_cache="${claim_home}/claim_cache_info"
+    last_claim_time_cache_key="${bp_account}_last_claim_time"
+    last_claim_check_cache_key="${bp_account}_claim_check_count"
     can_claim=false
     notify_tool="python ${work_home}/utils/notify.py"
     reward_tool="python ${work_home}/monitor/bp_status_monitor.py"
@@ -23,6 +25,7 @@ init_config() {
         log "please check client or wallet_pwd config"
         exit 0
     fi
+    init_cache
 }
 
 get_config() {
@@ -51,48 +54,57 @@ lock_wallet() {
     ${cleos} wallet lock
 }
 
-claim_rewards() {
-    unlock_wallet
-    claim_result=$(${cleos} push action eosio claimrewards '{"owner":"'${bp_account}'"}' -p ${bp_account}@claimer 2>&1)
-    if [ $? -gt 0 ];then
-        # remove color code
-        claim_result=$(echo "${claim_result}" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
-        notify "claim fail\n${claim_result}"
-        update_cache
-        exit 1
-    fi
-    log "${claim_result}"
-    bpay=$(echo "${claim_result}" | grep "${bp_account} <= eosio.token::transfer" | grep "eosio.bpay" | grep -Eo "[0-9]+\.[0-9]+ EOS")
-    vpay=$(echo "${claim_result}" | grep "${bp_account} <= eosio.token::transfer" | grep "eosio.vpay" | grep -Eo "[0-9]+\.[0-9]+ EOS")
-    [ "" == "${vpay}" ] && vpay="0 EOS"
-    [ "" == "${bpay}" ] && bpay="0 EOS"
-    claim_success
-}
-
 get_balance() {
     balance="`${cleos} get currency balance eosio.token ${bp_account}`"
 }
 
-claim_success() {
-    lock_wallet
-    get_balance
-    notify "claim success\nbpay:${bpay}\nvpay:${vpay}\nbalance:${balance}\nhttps://eosflare.io/account/${bp_account}"
-    can_claim=false
-    update_cache
+update_cache_file() {
+    local key=$1
+    local value=$2
+    info=`grep "${key}" ${last_claim_time_cache}`
+    if [ "" == "${info}" ]; then
+        echo ${key}=${value} >> ${last_claim_time_cache}
+    else
+        sed -i -e 's/'${key}'=.*/'${key}'='${value}'/g' ${last_claim_time_cache}
+    fi
 }
 
-update_cache() {
+update_claim_time_cache() {
     bp_info=`curl -sX POST ${api}/v1/chain/get_producers -d '{"limit":"1","lower_bound":"'${bp_account}'","json":"true"}'|jq '.rows[0]'`
     log "update claim cache: ${bp_info}"
     bp_name=`echo "${bp_info}"|jq '.owner'|sed 's/"//g'`
     [ "${bp_account}" != "${bp_name}" ] && log "${bp_account} get nothing." && return
     last_claim_time=`echo "${bp_info}"|jq '.last_claim_time'|sed 's/"//g'`
     last_claim_time_sec=`echo "${last_claim_time}/1000000"|bc`
-    info=`grep "${bp_account}_last_claim_time=" ${last_claim_time_cache}`
-    if [ "" == "${info}" ]; then
-        echo ${bp_account}_last_claim_time=${last_claim_time_sec} >> ${last_claim_time_cache}
+    update_cache_file ${last_claim_time_cache_key} ${last_claim_time_sec}
+}
+
+update_claim_check_cache() {
+    claim_check_count=$1
+    if [ "$1" == "" ];then
+        claim_check_count=$(get_cache_value ${last_claim_check_cache_key})
+        let claim_check_count=claim_check_count+1
+    fi
+    update_cache_file ${last_claim_check_cache_key} ${claim_check_count}
+}
+
+get_cache_value() {
+    local key=$1
+    value=`cat ${last_claim_time_cache}|grep ${key}|awk -F'=' '{print $2}'`
+    echo ${value}
+}
+
+init_cache() {
+    if [ ! -f "${last_claim_time_cache}" ];then
+        echo "${last_claim_time_cache_key}="> ${last_claim_time_cache}
+        echo "${last_claim_check_cache_key}=0">> ${last_claim_time_cache}
     else
-        sed -i -e 's/'${bp_account}'_last_claim_time=.*/'${bp_account}'_last_claim_time='${last_claim_time_sec}'/g' ${last_claim_time_cache}
+        if [ "$(get_cache_value ${last_claim_time_cache_key})" == "" ];then
+            update_claim_time_cache
+        fi
+        if [ "$(get_cache_value ${last_claim_check_cache_key})" == "" ];then
+            update_claim_check_cache 0
+        fi
     fi
 }
 
@@ -105,15 +117,19 @@ check_api() {
 }
 
 check_claim_time() {
-    [ ! -f "${last_claim_time_cache}" ] && echo "${bp_account}_last_claim_time="> ${last_claim_time_cache}
-    last_claim_time_sec=`cat ${last_claim_time_cache}|grep ${bp_account}_last_claim_time|awk -F'=' '{print $2}'`
+    last_claim_time_sec=$(get_cache_value ${last_claim_time_cache_key})
     if [ "${last_claim_time_sec}" == "" ];then
-        update_cache
-        exit
+        update_claim_time_cache
     fi
-    diff_sec=`echo "$(date "+%s")-${last_claim_time_sec}"|bc`
+    claim_check_count=$(get_cache_value ${last_claim_check_cache_key})
+    if [ ${claim_check_count} -lt 5 ];then
+        update_claim_time_cache
+    fi
+    last_claim_time_sec=$(get_cache_value ${last_claim_time_cache_key})
+    diff_sec=$(echo "$(date "+%s")-${last_claim_time_sec}"|bc)
     [ ${diff_sec} -gt ${seconds} ] && can_claim=true
     if [ ${can_claim} == false ]; then
+        update_claim_check_cache
         log "claim after $(echo ${seconds}-${diff_sec}|bc)s"
         exit 1
     fi
@@ -128,6 +144,33 @@ check_rewards() {
         log "${reward_pay} reward not enough"
         exit 1
     fi
+}
+
+claim_success() {
+    lock_wallet
+    get_balance
+    notify "claim success\nbpay:${bpay}\nvpay:${vpay}\nbalance:${balance}\nhttps://eosflare.io/account/${bp_account}"
+    can_claim=false
+    update_claim_time_cache
+    update_claim_check_cache 0
+}
+
+claim_rewards() {
+    unlock_wallet
+    claim_result=$(${cleos} push action eosio claimrewards '{"owner":"'${bp_account}'"}' -p ${bp_account}@claimer 2>&1)
+    if [ $? -gt 0 ];then
+        # remove color code
+        claim_result=$(echo "${claim_result}" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
+        notify "claim fail\n${claim_result}"
+        update_claim_time_cache
+        exit 1
+    fi
+    log "${claim_result}"
+    bpay=$(echo "${claim_result}" | grep "${bp_account} <= eosio.token::transfer" | grep "eosio.bpay" | grep -Eo "[0-9]+\.[0-9]+ EOS")
+    vpay=$(echo "${claim_result}" | grep "${bp_account} <= eosio.token::transfer" | grep "eosio.vpay" | grep -Eo "[0-9]+\.[0-9]+ EOS")
+    [ "" == "${vpay}" ] && vpay="0 EOS"
+    [ "" == "${bpay}" ] && bpay="0 EOS"
+    claim_success
 }
 
 process_claim() {
