7a86ff1fbdd3050e996ead5a2efbc2a1591f37f8
jiaoqsh <qiangsheng.jiao@bitmain.com>
2018-08-16 14:40:13 +0800

    update push prometheus metric

--- utils/metric.py
+++ utils/metric.py
@@ -38,15 +38,17 @@ class Metric:
 class Prometheus:
     headers = {'X-Requested-With': 'Python requests', 'Content-type': 'text/xml'}
     action = 'prometheus_metrics'
+    job = 'nodeos'
     hostname = socket.gethostname()
+    instance = hostname
 
     @staticmethod
-    def _submit(metric_name, metric_value):
+    def _submit(metric):
         host_port = MetricConfig.get_prometheus_host_port()
         if host_port is None or host_port is "":
             return
-        url = 'http://%s/metrics/job/%s' % (host_port, metric_name)
-        data = '%s\n' % metric_value
+        url = 'http://%s/metrics/job/%s/instance/%s' % (host_port, Prometheus.job, Prometheus.instance)
+        data = '%s\n' % metric
         http.post(Prometheus.action, url, data=data, headers=Prometheus.headers)
 
     @staticmethod
@@ -55,8 +57,8 @@ class Prometheus:
             value = '{hostname="%s"} %s' % (Prometheus.hostname, metric_value)
         else:
             value = '{producer_name="%s",hostname="%s"} %s' % (producer_name, Prometheus.hostname, metric_value)
-        value = metric_name + value
-        Prometheus._submit(metric_name=metric_name, metric_value=value)
+        metric = metric_name + value
+        Prometheus._submit(metric)
 
 
 class Alicms:
