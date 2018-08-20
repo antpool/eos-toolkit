5e694f3abc86aadc1f61cee150cf28e28d874a22
zhi.yang <zhi.yang@bitmain.com>
2018-08-20 11:01:43 +0800

    update auto_restart

--- monitor/auto_restart.sh
+++ monitor/auto_restart.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-work_home=$(cd `dirname $0`;cd ..;pwd)
-monitor_log="${work_home}/logs/monitor.log"
-notify_tool="python ${work_home}/utils/notify.py -m"
-logger="python ${work_home}/utils/logger.py"
-restart_command="${work_home}/node/start.sh"
-config_file="${work_home}/config/config.conf"
+init_config() {
+    work_home=$(cd `dirname $0`;cd ..;pwd)
+    monitor_log="${work_home}/logs/monitor.log"
+    notify_tool="python ${work_home}/utils/notify.py -m"
+    logger="python ${work_home}/utils/logger.py"
+    restart_command="${work_home}/node/start.sh"
+    config_file="${work_home}/config/config.conf"
 
-# minute
-restart_interval=$(get_config "auto_restart"|grep -Eo "[0-9]+")
-cpu_percent_limit=$(get_config "cpu_percent_limit")
-hostname=$(hostname)
+    # minute
+    restart_interval=$(get_config "auto_restart"|grep -Eo "[0-9]+")
+    cpu_percent_limit=$(get_config "cpu_percent_limit")
+    hostname=$(hostname)
+}
 
 get_config() {
     echo `egrep -m 1 "^$1" ${config_file} |sed -n 's/^[^=]*=\s* *\(.*\)\s* *$/\1/p'`
@@ -36,7 +38,7 @@ restart() {
 }
 
 check_cpu_percent() {
-    count=`echo "$[restart_interval]*2"|bc`
+    count=`echo "${restart_interval}*2"|bc`
     cpu_percent=`grep eos_cpu_percent ${monitor_log}|tail -${count}|grep -Eo "[0-9]+\.[0-9]+"| xargs | sed 's/ /+/g'`
     average=`echo "(${cpu_percent})/${count}"|bc`
     if [ `echo "${average}>${cpu_percent_limit}"|bc` -eq 1 ]; then
@@ -45,6 +47,7 @@ check_cpu_percent() {
 }
 
 main() {
+    init_config
     check_cpu_percent
 }
 
