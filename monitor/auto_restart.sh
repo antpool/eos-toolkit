940cc91f1dae8d12d57525024722f38f1c719c86
zhi.yang <zhi.yang@bitmain.com>
2018-08-22 13:36:29 +0800

    update auto restart

--- monitor/auto_restart.sh
+++ monitor/auto_restart.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+to_seconds() {
+   local config=$1
+   if [[ ${config} == *s ]];then
+       echo ${config}|grep -Eo "[0-9]+"
+   elif [[ ${config} == *m ]];then
+       echo $(echo ${config}|grep -Eo "[0-9]+")*60|bc
+   elif [[ ${config} == *h ]];then
+       echo $(echo ${config}|grep -Eo "[0-9]+")*3600|bc
+   fi
+}
+
 init_config() {
     work_home=$(cd `dirname $0`;cd ..;pwd)
     monitor_log="${work_home}/logs/monitor.log"
@@ -8,8 +19,11 @@ init_config() {
     restart_command="${work_home}/node/start.sh"
     get_config="${work_home}/config/config.sh"
 
-    # minute
-    restart_interval=$(${get_config} "auto_restart"|grep -Eo "[0-9]+")
+    process_monitor_config=$(${get_config} "process_monitor"|grep -Eo "[0-9]+.*")
+    auto_restart_config=$(${get_config} "auto_restart"|grep -Eo "[0-9]+.*")
+    process_monitor_seconds=$(to_seconds ${process_monitor_config})
+    auto_restart_seconds=$(to_seconds ${auto_restart_config})
+
     cpu_percent_limit=$(${get_config} "cpu_percent_limit")
     hostname=$(hostname)
 }
@@ -24,7 +38,7 @@ notify() {
 }
 
 restart() {
-    notify "${hostname} last ${restart_interval}m cpu_percent is ${average}, start restart(${restart_command})..."
+    notify "${hostname} last ${auto_restart_config} cpu_percent is ${average}, start restart(${restart_command})..."
     ${restart_command}
     if [ $? -eq 0 ]; then
         notify "${hostname} restart success"
@@ -34,10 +48,18 @@ restart() {
 }
 
 check_cpu_percent() {
-    count=`echo "${restart_interval}*2"|bc`
-    cpu_percent=`grep eos_cpu_percent ${monitor_log}|tail -${count}|grep -Eo "[0-9]+\.[0-9]+"| xargs | sed 's/ /+/g'`
-    average=`echo "(${cpu_percent})/${count}"|bc`
-    log "${hostname} last ${restart_interval}m cpu_percent is ${average}"
+    lines_count=`echo "${auto_restart_seconds}/${process_monitor_seconds}"|bc`
+    if [ ${lines_count} -eq 0 ];then
+        log "auto restart interval too small"
+        exit 1
+    fi
+    cpu_percent=`grep eos_cpu_percent ${monitor_log}|tail -${lines_count}|grep -Eo "[0-9]+\.[0-9]+"| xargs | sed 's/ /+/g'`
+    if [ "${cpu_percent}" == "" ];then
+        log "not found cpu monitor data"
+        exit 1
+    fi
+    average=`echo "(${cpu_percent})/${lines_count}"|bc`
+    log "${hostname} last ${auto_restart_config} cpu_percent is ${average}"
     if [ `echo "${average}>${cpu_percent_limit}"|bc` -eq 1 ]; then
         restart
     fi
