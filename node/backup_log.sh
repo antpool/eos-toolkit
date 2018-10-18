5e7aab1f66198789f657bb2c1ad4d0155eb0b21c
zhi-cike <zhi_cike@163.com>
2018-10-18 18:41:35 +0800

    add log backup scripts

---
+++ node/backup_log.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+current_home=$(cd `dirname $0`;pwd)
+work_home=$(cd ${current_home};cd ../;pwd)
+get_config="${work_home}/config/config.sh"
+eos_home=$(${get_config} "eos_home")
+log_home="${eos_home}/logs"
+logger="python ${work_home}/utils/logger.py"
+
+stdout_log="stdout.txt"
+stderr_log="stderr.txt"
+suffix=$(date "+%Y%m%d_%H%M")
+
+log() {
+    ${logger} -m "$@"
+}
+
+clear_history_log() {
+    find_history_command="find ${log_home} -ctime +30 -type f -name '$1*'"
+    echo ${find_history_command}
+    history_log=$(${find_history_command})
+    if [ "${history_log}" == "" ];then
+        return
+    fi
+    log "clear history log: $(${find_history_command})"
+    ${find_history_command} | xargs rm -f
+}
+
+backup_log() {
+    if [ "" == "$1" ];then
+        return
+    fi
+    clear_history_log $1
+    log_file="${log_home}/$1"
+    if [ -f "${log_file}" ] && [ -s "${log_file}" ];then
+        cp -f ${log_file} "${log_file}_${suffix}"
+    fi
+}
+
+backup_log ${stderr_log}
+backup_log ${stdout_log}
--- node/start.sh
+++ node/start.sh
@@ -10,6 +10,7 @@ config_dir="${eos_home}/config"
 log_dir="${eos_home}/logs"
 
 ${current_home}/stop.sh
+${current_home}/backup_log.sh
 
 ${nodeos} --data-dir ${data_dir} --config-dir ${config_dir} "$@" > ${log_dir}/stdout.txt 2> ${log_dir}/stderr.txt &  echo $! > ${log_dir}/nodeos.pid
 [ $? -eq 0 ] && echo "Nodeos started successfully" || echo "Nodes start failed"
